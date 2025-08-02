12e422ee9b2400e85f3a817ab4d4c6a6b6c8dcb4
AratrikSarkar <aratriksarkar02@gmail.com>
2025-08-03 00:32:26 +0530

    Major changes 1

---
+++ Model1.py
@@ -0,0 +1,115 @@
+import torch
+import torch.nn as nn
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class Linear(nn.Module):
+    def __init__(self,fan_in,fan_out,bias=False):
+        super().__init__()
+        self.W=nn.Parameter(torch.randn((fan_out,fan_in)))
+        self.b=nn.Parameter(torch.randn(fan_out)) if bias==True else None
+    
+    def forward(self, x):
+        out= x @ self.W.T
+        if self.b is not None:
+            out=out+self.b
+        return  out
+    
+class ReLu(nn.Module):
+    def __init__(self,inplace=False):
+        super().__init__()
+        self.inplace=inplace
+
+    def forward(self,x):
+        if(self.inplace):
+            return torch.clamp_(x,min=0.0)      # modifies x
+        else:
+            return torch.clamp(x,min=0.0)       # x remains unchanged
+        
+class Sigmoid(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, x):
+        out=torch.empty_like(x)
+        positive=x>=0
+        negative=~positive
+
+        # For x ≥ 0:
+        out[positive]=1/(1+torch.exp(-x[positive]))
+
+        # For x < 0: reformulated to avoid large exp
+        exp_x=torch.exp(x[negative])
+        out[negative]=exp_x/(1+exp_x)
+        return out
+
+class TanH(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self,x):
+        out=torch.empty_like(x)
+        positive=x>=0
+        negative=~positive
+        out[positive]=1-(2/(torch.exp(2*x[positive])+1))
+        out[negative]=(2/(torch.exp(-2*x[negative])+1))-1
+        return out
+    
+class RNN(nn.Module):
+    def __init__(self,fan_in,fan_out,bias=True):
+        super().__init__()
+        self.input_size=fan_in
+        self.hidden_size=fan_out
+        self.i2h=Linear(fan_in,fan_out,bias)         #input ----> hidden
+        self.h2h=Linear(fan_out,fan_out,bias)        #hidden ---> hidden
+        self.activation=TanH()
+    
+    def forward(self, x, h0=None):
+        """
+        x: (seq_len, batch_size, input_size)
+        h0: (batch_size, hidden_size)
+        """
+
+        seq_len, batch_size,_=x.shape
+        
+        if h0 is None:
+            h_t=torch.zeros(batch_size, self.hidden_size,device=x.device)
+        else:
+            h_t=h0
+        
+        outputs=[]
+        for t in range(seq_len):
+            x_t=x[t]            #(batch_size,input_size)
+            # (batch_size,input_size)  @ (input_size,hidden_size) ----> (batch_size,hidden_size)
+            # (batch_size,hidden_size)  @ (hidden_size,hidden_size) ----> (batch_size,hidden_size)
+            h_t=self.activation(self.i2h(x_t)+self.h2h(h_t)) # (batch_size,hidden_size) + (batch_size,hidden_size)
+            outputs.append(torch.unsqueeze(h_t,dim=0))                  #(batch_size,input_size) ----> (1,batch_size,input_size)
+        
+        out=torch.cat(outputs,dim=0) #concatenating all the (1,batch_size,input_size) into (seq_len,batch,input_size)
+        return  out,h_t.unsqueeze(0)
+
+q=Linear(3,4)
+m=q.to(device)
+x=torch.randn((4,3)).to(device)
+print(x.shape)
+print(m(x))
+
+a=torch.randn((1,4)).to(device)
+print(a,torch.relu(a))
+k=ReLu().to(device)
+print(k(a),a)
+
+x = torch.tensor([-1000.0, 0.0, 1000.0]).to(device=device)
+k=Sigmoid().to(device=device)
+print("Stable:", k(x)) 
+
+x = torch.tensor([-1000.0, 0.0, 1000.0]).to(device=device)
+k=TanH().to(device=device)
+print("Stable:", k(x)) 
+
+seq_len, batch_size, input_size, hidden_size = 5, 3, 10, 8
+x = torch.randn(seq_len, batch_size, input_size).to(device=device)
+rnn = RNN(input_size, hidden_size).to(device=device)
+
+out, hn = rnn(x)
+print(out.shape)  # torch.Size([5, 3, 8])
+print(hn.shape)   # torch.Size([1, 3, 8])
